b29a68a8116c0215c43740027c6c32ecd1259a50
George Yarr <2553638y@student.gla.ac.uk>
2022-07-14 09:44:43 +0100

    for loop fahr to celcius

--- Chapter1/main.c
+++ Chapter1/main.c
@@ -38,9 +38,22 @@ void celciusToFahr(){
     }
 }
 
+void fahrToCelciusForLoop(){
+    for (int fahr = 300; fahr >= 0 ; fahr -= 20) {
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
+}
+
+
+
 int main() {
     printf("Hello, World!\n");
-    fahrToCelciusHeader();
-    celciusToFahr();
+    /* function calls
+     * ideally would have separate files to be called externally
+     * but don't want to mess with auxiliary functions just yet
+     */
+    //fahrToCelciusHeader();
+    //celciusToFahr();
+    fahrToCelciusForLoop();
     return 0;
 }
