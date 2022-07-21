ce0f5487e3cc4e19b4661b7a50d135db2cca3b54
George Yarr <2553638y@student.gla.ac.uk>
2022-07-21 22:10:44 +0100

    reverse string exercise

--- Chapter 1_afterFunctions/reverseInputLine.c
+++ Chapter 1_afterFunctions/reverseInputLine.c
@@ -3,23 +3,20 @@
 #define MAXLINE 1000 /* maximum input line length */
 
 int get_line(char line[], int maxline);
-
+void reverse(char string[]);
 /* print the longest input line */
 int main(void) {
-    int len; /* current line length */
     char line[MAXLINE]; /* current input line */
 
-    while ((len = get_line(line, MAXLINE)) > 0) {
-        if (len > 1 && line[0] != '\n') {
-            printf("%s\n", line);
-        }
+    while(get_line(line, MAXLINE) > 0){
+        reverse(line);
     }
     return 0;
 }
 
 
 /* get_line: read a line into s, return length */
-/*
+
 int get_line(char s[], int lim) {
     int c, i, l;
 
@@ -44,5 +41,14 @@ void copy(char to[], char from[]) {
     while ((to[i] = from[i]) != '\0')
         ++i;
 }
-*/
 
+
+void reverse(char string[]){
+    int string_length = 0;
+    for (int i = 0; string[i] != '\0'; ++i){
+        ++string_length;
+    }
+    for (int j = string_length; j >= 0 ; --j) {
+        putchar(string[j]);
+    }
+}
